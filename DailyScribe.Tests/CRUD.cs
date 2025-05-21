e07950fbf1c72b84b1b2323072b40bcfd0a4f4cb
Mikołaj <mikolajkocik@onet.pl>
2025-05-21 20:32:29 +0200

    crud tests init

---
+++ DailyScribe.Tests/CRUD.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyScribe.Tests
+{
+    public class CRUD
+    {
+        [Fact]
+        public void Add_Entry_Should_Return_True()
+        {
+
+        }
+        
+        [Fact]
+        public void Read_Entry_Should_Return_True()
+        {
+
+        }
+      
+        [Fact]
+        public void Edit_Entry_Should_Return_True()
+        {
+
+        }
+        
+        [Fact]
+        public void Delete_Entry_Should_Return_True()
+        {
+
+        }
+    }
+}
